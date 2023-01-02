44798f96f18cea0d3c0d538ae415643c4a705e10
Johannes Tykesson <jochedator@gmail.com>
2023-01-02 21:17:10 +0100

    Updates to image_utils

--- src/image_utils.rs
+++ src/image_utils.rs
@@ -1,4 +1,4 @@
-use image::{ GenericImageView, ImageBuffer, Luma };
+use image::{ GenericImageView, ImageBuffer, Luma, GrayImage };
 
 pub fn read_image(_path: String) -> Vec<u8> {
     let img = image::open(_path).expect("File not found!");
@@ -23,4 +23,19 @@ pub fn read_image_grayscale(_path: String) -> Vec<f32> {
         pixel_values.push((*it as f32) / 255_f32)
     );
     return pixel_values;
+}
+
+pub fn read_image_dimensions(_path: String) -> (u32, u32) {
+    return image::open(_path).expect("File not found!").dimensions();
+}
+
+pub fn create_grayscale_image(_image_vec: &Vec<f32>, _width: u32, _height: u32) {
+    let mut image: GrayImage = ImageBuffer::new(_width, _height);
+    for width_index in 0.._width-1 {
+        for height_index in 0.._height-1 {
+            let idx = (width_index*height_index+height_index) as usize;
+            *image.get_pixel_mut(width_index, height_index) = image::Luma([(_image_vec[idx] * 255_f32) as u8]);
+        }
+    }
+    image.save("output.png").unwrap();
 }
