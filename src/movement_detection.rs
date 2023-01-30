613f663d00ebfeef7a7a4b0da197a8b33ed78b53
Johannes Tykesson <jochedator@gmail.com>
2023-01-30 18:22:49 +0100

    Add missing line

--- src/movement_detection.rs
+++ src/movement_detection.rs
@@ -5,6 +5,7 @@ pub fn calculate_diff_pixelwise(image1: &Vec<f32>, image2: &Vec<f32>) -> Vec<f32
     let mut diff_vec: Vec<f32> = vec![];
     for n in 0..image1.len()-1 {
         let diff = image1[n] - image2[n];
+        diff_vec.push(diff.abs());
     }
     return diff_vec;
 }
